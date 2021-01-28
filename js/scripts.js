c0fcd556ab0c4bf1029d04103d459e9c3aece5ff
Levi Kohler <kohler.la01@gmail.com>
2021-01-28 10:56:04 -0800

    add js functionality so the score will increase by whatever number is rolled, also add a hold button
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 // Business Logic
 
 function Dice(roll) {
-this.roll = roll;
+  this.roll = roll;
 }
 
 function Player(name) {
   this.name = name
   this.turnScore = 0;
   this.totalScore = 0;
+}
+
+Player.prototype.addScore = function() {
+  this.totalScore += score;
 }
 
 Dice.prototype.rollDice = function() {
@@ -23,17 +27,21 @@ if (dice > 1) {
 
 // UI Logic
 $(document).ready(function() {
-
+  let dice = new Dice();
+  let player1 = new Player();
+  let player2 = new Player();
+  let player1Total = 0;
+  let player2Total = 0;
+  
   $("#go").click(function() {
-  let roll1 = new Dice();
-  let roll1loss = function() {
-    let roll = roll1.rollDice();
-    if (roll === false) {
-      return "GAME OVER MAAAN"
-    } else {
-      return roll;
+    let roll1 = function() {
+      let roll = dice.rollDice();
+      if (roll === false) {
+        return "GAME OVER MAAAN"
+      } else {
+        return player1Total += roll;
+      }
     }
-  }
-  $("#output").text(roll1loss);
+  $("#output").text(roll1);
   });
 });
