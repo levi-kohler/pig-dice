4ca53c8423908decbb27ddaf13e1705f2d054726
Levi Kohler <kohler.la01@gmail.com>
2021-01-28 09:14:04 -0800

    add HTML outline and README
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-// User outcomes:
+// Player outcomes:
 
 // Roll a 1: End turn
 
-// Roll a 2 - 5: Add to round total
+// Roll a 2 - 6: Player's turn continues and dice total is added to round total
