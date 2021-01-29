0081897113d4eaa51d97e7df1f0a4b3471e743f7
Levi Kohler <kohler.la01@gmail.com>
2021-01-28 16:26:59 -0800

    add hide/show features for buttons so a player can only play on their turn
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -42,9 +42,14 @@ $(document).ready(function() {
   $("button#player1roll").click(function(event) {
     player1.roll = rollDice();
     $("#diceroll1").text(player1.roll);
-    player1.rollOne()
+    player1.rollOne();
     $("#roundscore1").text(player1.roundScore);
-
+    if (player1.roundScore === 0) {
+      $("button#player1roll").hide();
+      $("button#player1hold").hide();
+      $("button#player2roll").show();
+      $("button#player2hold").show();
+      }
   });
 
   $("button#player1hold").click(function(event) {
@@ -53,25 +58,35 @@ $(document).ready(function() {
     $("#roundscore1").empty();
     $("#diceroll1").empty();
     player1.checkWin();
-  })
-
+    $("button#player1roll").hide();
+    $("button#player1hold").hide();
+    $("button#player2roll").show();
+    $("button#player2hold").show();
+    
+  });
   $("button#player2roll").click(function(event) {
     player2.roll = rollDice();
     $("#diceroll2").text(player2.roll);
     player2.rollOne()
     $("#roundscore2").text(player2.roundScore);
-
+    if (player2.roundScore === 0) {
+      $("button#player1roll").show();
+      $("button#player1hold").show();
+      $("button#player2roll").hide();
+      $("button#player2hold").hide();
+    }
   });
-
   $("button#player2hold").click(function(event) {
     player2.hold();
     $("#totalscore2").text(player2.totalScore);
     $("#roundscore2").empty();
     $("#diceroll2").empty();
     player2.checkWin();
+    $("button#player1roll").show();
+    $("button#player1hold").show();
+    $("button#player2roll").hide();
+    $("button#player2hold").hide();
   })
-
-
 });
 
 
